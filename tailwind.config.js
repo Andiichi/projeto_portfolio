96fab20d71f1b17d1b845c887db94d1ad068d12d
Andiichi <andrezza.n.dasilva@gmail.com>
2024-09-24 20:33:00 -0300

    algumas alterações

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,15 @@ module.exports = {
     colors: {
       'color-primary': '#E6CBB6',
       'color-secondary': '#BB7D58',
-      'color-primary-tittle': '#422815',
-      'color-secondary-subtittle': '#D16210',
+      'color-primary-title': '#422815',
+      'color-secondary-subtitle': '#D16210',
       'color-detalhes': '#F5F6F5',
+      'red': {
+        500: '#ef4444',
+        600: '#dc2626',
+        700: '#b91c1c',
+        800: '#991b1b',
+      }
     },
 
     fontFamily: {
